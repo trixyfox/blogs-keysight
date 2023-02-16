e78f964e7a2537d2ae9418ac971b2552abef6305
Trixy Fox <trixyfox@gmail.com>
2023-02-16 10:01:19 -0700

    Initial Blocl

--- blocks/related-content/related-content.js
+++ blocks/related-content/related-content.js
@@ -1,4 +1,20 @@
+import {
+  createElement,
+} from '../../scripts/scripts.js';
+
 export default function decorate(block) {
-  const cols = [...block.firstElementChild.children];
-  block.classList.add(`columns-${cols.length}-cols`);
+  /* change to ul, li  */
+  const colWrapper = block.firstElementChild;
+  colWrapper.classList.add('cols');
+  const cols = [...colWrapper.children];
+  cols.forEach((col) => {
+    const list = createElement('ul');
+    col.querySelectorAll('a').forEach((a) => {
+      const li = createElement('li');
+      a.classList.remove('button');
+      li.append(a);
+      list.append(li);
+    });
+    col.append(list);
+  });
 }
